604341dee3b4484341fbe6114841e5ea99a1398b
norre-daroy <norrej.daroy@gmail.com>
2024-02-20 21:58:39 -0500

    Add mui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
 import '@/app/ui/global.css';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
 import { inter } from '@/app/ui/fonts';
+import theme from '@/app/theme';
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children} </ThemeProvider>{' '}
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
